7b21e3f445a21aa6d0596c0d79f9ecd7d956e183
Jéssica Souza <jessicamosouza@users.noreply.github.com>
2023-08-11 12:45:26 +0000

    add quicksort code

---
+++ cap-4-quicksort/quicksort.py
@@ -0,0 +1,11 @@
+def quicksort(arr):
+    if len(arr) < 2:
+        return arr
+    else: 
+        pivot = arr[0]
+        smaller = [i for i in arr[1:] if i <= pivot]
+        larger = [i for i in arr[1:] if i > pivot]
+        return quicksort(smaller) + [pivot] + quicksort(larger)
+
+
+print(quicksort([42, 11, 85, 7, 33, 19, 58]))
